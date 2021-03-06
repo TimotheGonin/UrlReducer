4d435b8addd0db1452079b5e5fa48f34a135f6b0
Timothe Gonin <gonin.timothe@gmail.com>
2021-03-06 19:56:44 +0100

    reset CSS, ajout background et paramatres #hello, squelette de base suite

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/default.css">
+    <link rel="icon" type="image/png" href="pictures/favico.png"/>
+    <title>Url Reducer</title>
+</head>
+<body>
+<!-- HEADER -->
+    <header id="hello">
+        <div>
+            <img src="" alt="">
+        </div>
+        <h1></h1>
+        <h2></h2>
+        <form action="">
+            <input type="url" name="">
+            <button type="submit">RACCOURCIR</button>
+        </form>
+    </header>
+<!-- SECTION -->
+    <section>
+        <h2></h2>
+    </section>
+<!-- FOOTER -->
+    <footer>
+        <div><img src="" alt=""></div>
+    </footer>
+</body>
+</html>
